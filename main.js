4913200226110acf37318cf13f1bbea64d85375a
lc00 <leannecheung00@gmail.com>
2014-06-27 12:50:01 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+$(document).on('ready', function() {
+  	$('.text-box').hover(function(){
+  		$(this).css('background-color', 'orange')
+  		
+  	});
+
+
+	$('.text-box').on('click', function(){
+		console.log("snow")
+
+
+	})
+
+
+
+
+});
+
